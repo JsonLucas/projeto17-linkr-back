cea465bcad1b940ee7ae9c1fdfa4572d07174ec0
Eduardo <eduyudji@gmail.com>
2022-06-24 12:12:02 -0300

    add Link Preview into publish

--- controllers/publishController.js
+++ controllers/publishController.js
@@ -1,5 +1,6 @@
 import { getUserById } from "../database/queries/retrieve/users.js";
 import postPublish from "./../database/queries/insert/publish.js";
+import getMetaData from "metadata-scraper";
 
 async function publishController(req, res){
     const { userId } = res.locals;
@@ -7,7 +8,8 @@ async function publishController(req, res){
     try{
         const user = await getUserById(userId);
         if(user.rowCount > 0){
-            const insertionPost = await postPublish(link, user.rows[0].id, commenter);
+            const data = await getMetaData(link)
+            const insertionPost = await postPublish(link, user.rows[0].id, commenter, data.title, data.image, data.icon, data.description);
             if(insertionPost.rowCount > 0){
                 res.sendStatus(201);
                 return;
@@ -17,7 +19,7 @@ async function publishController(req, res){
         }
         res.sendStatus(401);
     } catch (e) {
-        console.log(e);
+        console.log(e)
         res.status(500).send('Não foi possível se conectar com o BD');
     }
 }
